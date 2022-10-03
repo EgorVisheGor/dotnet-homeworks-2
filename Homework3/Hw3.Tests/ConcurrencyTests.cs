7de73739453c492768250fd0495ae84cde51d1bf
Egor <korolkov-nat@mail.ru>
2022-10-03 16:40:08 +0300

    Coverage Fix

--- Homework3/Hw3.Tests/ConcurrencyTests.cs
+++ Homework3/Hw3.Tests/ConcurrencyTests.cs
@@ -31,7 +31,7 @@ public class ConcurrencyTests
         Assert.Equal(expected, Concurrency.Index);
     }
     
-    [Fact(Skip = "Undeterminated behaviour")]
+    [Fact(Skip = "Undetermined behaviour")]
     public void EightThreads_100KIterations_RaceIsReproduced()
     {
         var expected = Concurrency.Increment(8, 100_000);
