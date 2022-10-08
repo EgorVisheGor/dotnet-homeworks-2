61067c9251b508dad7a6c78ab2a3501d2a02e479
Egor <korolkov-nat@mail.ru>
2022-10-09 01:07:42 +0300

    Code update

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -9,10 +9,7 @@ type CalcOptions =
       arg2: float
       operation: CalculatorOperation }
 
-let isArgLengthSupported (args: string []) =
-    match args.Length with
-    | 3 -> true
-    | _ -> false
+let isArgLengthSupported (args: string []) = args.Length = 3
 
 let parseOperation (arg: string) =
     match arg with
@@ -21,20 +18,19 @@ let parseOperation (arg: string) =
     | "*" -> CalculatorOperation.Multiply
     | "/" -> CalculatorOperation.Divide
     | _ -> raise (ArgumentException("Введенная операция не распознается"))
+    
+let parseDouble (str: string) =
+    match Double.TryParse str with
+    | true, str -> str
+    | _ -> raise (ArgumentException"Элемент не является числом")
 
 let parseCalcArguments (args: string []) =
     if not (isArgLengthSupported args) then
         raise (ArgumentException("Введенные данные некорректны"))
 
-    let val1 = match System.Double.TryParse args[0] with
-      | true, a -> a
-      | _ -> raise (ArgumentException "Введенные данные некорректны")
-      
+    let val1 = parseDouble args[0]     
     let operation = parseOperation args[1]
-    
-    let val2 = (match System.Double.TryParse args[2] with
-      | true, a -> a
-      | _ -> raise (ArgumentException "Введенные данные некорректны"))
+    let val2 = parseDouble args[2] 
     
     {
         arg1 = val1
