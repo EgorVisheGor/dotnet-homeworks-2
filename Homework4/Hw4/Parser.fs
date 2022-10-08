e7a4bf6b4baaa889e862b11c80ad271e5b24eac2
Egor <korolkov-nat@mail.ru>
2022-10-08 14:36:12 +0300

    HW4

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -4,17 +4,40 @@ open System
 open Hw4.Calculator
 
 
-type CalcOptions = {
-    arg1: float
-    arg2: float
-    operation: CalculatorOperation
-}
+type CalcOptions =
+    { arg1: float
+      arg2: float
+      operation: CalculatorOperation }
 
-let isArgLengthSupported (args : string[]) =
-    NotImplementedException() |> raise
+let isArgLengthSupported (args: string []) =
+    match args.Length with
+    | 3 -> true
+    | _ -> false
 
-let parseOperation (arg : string) =
-    NotImplementedException() |> raise
+let parseOperation (arg: string) =
+    match arg with
+    | "+" -> CalculatorOperation.Plus
+    | "-" -> CalculatorOperation.Minus
+    | "*" -> CalculatorOperation.Multiply
+    | "/" -> CalculatorOperation.Divide
+    | _ -> raise (ArgumentException("Введенная операция не распознается"))
+
+let parseCalcArguments (args: string []) =
+    if not (isArgLengthSupported args) then
+        raise (ArgumentException("Введенные данные некорректны"))
+
+    let val1 = match System.Double.TryParse args[0] with
+      | true, a -> a
+      | _ -> raise (ArgumentException "Введенные данные некорректны")
+      
+    let operation = parseOperation args[1]
     
-let parseCalcArguments(args : string[]) =
-    NotImplementedException() |> raise
+    let val2 = (match System.Double.TryParse args[2] with
+      | true, a -> a
+      | _ -> raise (ArgumentException "Введенные данные некорректны"))
+    
+    {
+        arg1 = val1
+        operation = operation
+        arg2 = val2
+    }
--- Homework4/Hw4/Program.fs
+++ Homework4/Hw4/Program.fs
@@ -1,1 +1,5 @@
-﻿raise (System.NotImplementedException())
+﻿open Hw4
+
+let main args =
+    let example = Parser.parseCalcArguments args
+    printf $"{Calculator.calculate example.arg1 example.operation example.arg2}"
