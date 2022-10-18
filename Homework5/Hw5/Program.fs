52f8aac71592b003340a8317476565175ce93e8b
Egor <korolkov-nat@mail.ru>
2022-10-18 22:21:21 +0300

    Added EntryPoint and a message of successful execution

--- Homework5/Hw5/Program.fs
+++ Homework5/Hw5/Program.fs
@@ -2,8 +2,14 @@
 open Hw5
 
 let succesPrint(val1, operation, val2) =
-    printfn $"{Calculator.calculate val1 operation val2}"
+    Error Message.SuccessfulExecution
+    Calculator.calculate val1 operation val2
+    
+    
+[<EntryPoint>]
 let main (args : string[]) =
     match Parser.parseCalcArguments args with
-    | Ok res -> succesPrint res
-    | Error message -> printf $"{message}" 
+    | Ok res -> printf $"{succesPrint res}"
+    | Error message -> printf $"{message}"
+    0
+    
