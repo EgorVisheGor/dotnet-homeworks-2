7eaf95c32e5b15660139df284b7cb1c95a939c99
Egor <korolkov-nat@mail.ru>
2022-12-10 23:11:29 +0300

    Done

---
+++ Homework6/Hw6/Parser.fs
@@ -0,0 +1,44 @@
+﻿module Hw6.Parser
+
+open System
+open System.Diagnostics.CodeAnalysis
+open System.Globalization
+open Hw6.Calculator
+open Hw6.Message
+
+
+[<ExcludeFromCodeCoverage>]
+let isArgLengthSupported (args:string[]): Result<_, _> =
+    match args.Length with
+    | 3 -> Ok args
+    | _ -> Error Message.WrongArgLength 
+    
+[<ExcludeFromCodeCoverage>]
+let inline isOperationSupported (arg1, operation, arg2): Result<('a * CalculatorOperation * 'b), Message> =
+    match operation with
+    | "Plus" -> Ok (arg1, CalculatorOperation.Plus, arg2)
+    | "Minus" -> Ok (arg1, CalculatorOperation.Minus, arg2)
+    | "Multiply" -> Ok (arg1, CalculatorOperation.Multiply, arg2)
+    | "Divide" -> Ok (arg1, CalculatorOperation.Divide, arg2)
+    | _ -> Error Message.WrongArgFormatOperation
+
+let parseArgs (args: string[]): Result<('a * CalculatorOperation * 'b), Message> =
+   match Double.TryParse(args[0], NumberStyles.Float, CultureInfo.InvariantCulture) with
+   | true, arg1  -> match Double.TryParse(args[2], NumberStyles.Float, CultureInfo.InvariantCulture) with
+                    |true, arg2 -> isOperationSupported (arg1, args[1], arg2)
+                    |_ -> Error Message.WrongArgFormatForValue2
+   |_ -> Error Message.WrongArgFormatForValue1
+
+[<ExcludeFromCodeCoverage>]
+let inline isDividingByZero (arg1 : ^a, operation, arg2 : ^a): Result<('a * CalculatorOperation * 'b),Message> =
+    match operation, float arg2 with
+    | CalculatorOperation.Divide, 0.0 -> Error Message.DivideByZero
+    | _ -> Ok (arg1, operation, arg2)
+    
+let parseCalcArguments (args: string[]): Result<'a, 'b> =
+    MaybeBuilder.maybe{
+        let! lengthSupported = isArgLengthSupported args
+        let! parsedArgs = parseArgs lengthSupported
+        let! result = isDividingByZero parsedArgs
+        return result
+    }   
--- Homework6/Hw6/Program.fs
+++ Homework6/Hw6/Program.fs
@@ -7,12 +7,32 @@ open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
+open Hw6.Message
 
+[<CLIMutable>]
+type Values =
+    {
+        value1: string
+        operation: string
+        value2: string
+    }
+    
+    
+[<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
 let calculatorHandler: HttpHandler =
     fun next ctx ->
-        let result: Result<string, string> = raise(NotImplementedException())
-
-        match result with
+        
+        let values = ctx.BindQueryString<Values>()
+        let args = [|values.value1;values.operation;values.value2|]
+        let parsedargs = Parser.parseCalcArguments args
+        
+        let result =
+            match parsedargs with
+            | Ok ok -> Ok (ok |||> Calculator.calculate)
+            | Error error -> Error error
+        let message = convertMessageToString(result, args)
+        
+        match message with
         | Ok ok -> (setStatusCode 200 >=> text (ok.ToString())) next ctx
         | Error error -> (setStatusCode 400 >=> text error) next ctx
 
@@ -20,6 +40,7 @@ let webApp =
     choose [
         GET >=> choose [
              route "/" >=> text "Use //calculate?value1=<VAL1>&operation=<OPERATION>&value2=<VAL2>"
+             route "/calculate" >=> calculatorHandler
         ]
         setStatusCode 404 >=> text "Not Found" 
     ]
