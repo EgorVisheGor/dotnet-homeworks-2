3560261ee12d104fe16f9ba65529e4fdebc60372
Egor <korolkov-nat@mail.ru>
2022-12-11 22:20:49 +0300

    Codecoverage fix #1

---
+++ Homework9/Hw9.Tests/CalculatorVisitorTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq.Expressions;
+using Hw9.ErrorMessages;
+using Xunit;
+
+namespace Hw9.Tests;
+
+public class CalculatorVisitorTests
+{
+    private CalcVisitorImpl _calculatorVisitor;
+    private ExpressionConverter _converter;
+
+    private Dictionary<BinaryExpression, string> unsupportedOperations = new()
+    {
+        {Expression.AddChecked(Expression.Constant((double)5), Expression.Constant((double)5)),
+            "calculator doesn't support this operation"},
+        {Expression.MultiplyChecked(Expression.Constant((double)5), Expression.Constant((double)5)),
+            "calculator doesn't support this operation"},
+        {Expression.SubtractChecked(Expression.Constant((double)11), Expression.Constant((double)5)),
+            "calculator doesn't support this operation"},
+        {Expression.Divide(Expression.Constant((double)10), Expression.Constant((double)0)), 
+            MathErrorMessager.DivisionByZero}
+    };
+
+    public CalculatorVisitorTests(){
+        _calculatorVisitor = new CalcVisitorImpl();
+    }
+
+    [Fact]
+    public async Task Visit_CalculatorVisitor_Error()
+    {
+        foreach (var (binaryExpression, message) in unsupportedOperations)
+        {
+            try
+            {
+                _converter = new ExpressionConverter();
+                var expressionsMap = _converter.ExpressionDictionary(binaryExpression);
+                var result = await _calculatorVisitor.CalculatorVisitBinary(expressionsMap);
+            }
+            catch (Exception ex)
+            {
+                Assert.Equal(message,ex.Message);
+            }
+        }
+    }
+}
